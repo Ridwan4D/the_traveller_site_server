f8ac603d71c2cc086c85fe1644fe379885b52dda
Ridwanul Islam <ridwanul161@gmail.com>
2024-11-23 19:02:29 +0300

    delete api created for slider collection

--- index.js
+++ index.js
@@ -207,6 +207,13 @@ async function run() {
       const result = await sliderCollection.insertOne(bannerInfo);
       res.send(result);
     });
+
+    app.delete("/sliders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await sliderCollection.deleteOne(query);
+      res.send(result);
+    });
     // ========================================   slider type collection end    ========================================
 
     // Send a ping to confirm a successful connection
